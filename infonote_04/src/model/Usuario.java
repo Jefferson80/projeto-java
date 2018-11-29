36751a8d52eafcfbe8bd299188526fafc3d83fd9
Jefferson80 <jeffersonblack@gmail.com>
2018-11-29 21:23:35 -0200

    infonote_04

--- infonote_04/src/model/Usuario.java
+++ infonote_04/src/model/Usuario.java
@@ -80,6 +80,28 @@ public class Usuario {
 
 	}
 
+	// inserir o pedido no vetor
+	public boolean inserirPedido(Pedido pedido){
+	for (int i = 0; i < pedidos.length; i++) {
+	if (pedidos[i] == null) {
+	pedidos[i] = pedido;
+	return true;
+	}
+	}
+	return false;
+	}
+	// inserir o endereco no vetor
+	public boolean inserirEndereco (Endereco end) {
+	for (int i = 0; i < enderecos.length; i++) {
+	if (enderecos[i] == null){
+	enderecos[i] = end;
+	return true;
+	}
+	}
+	return false;
+	}
+	
+	
 	public void mostrar() {
 
 		System.out.println("`\n\nUsuário:\n");
